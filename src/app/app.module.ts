be55d4b56b75f89db9bf00a156dc335cb80e863b
Gianni Narracci <gnarracci@gmail.com>
2018-09-26 18:40:05 -0400

    Creacion de Componentes de la App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { PrivatePageComponent } from './componentes/private-page/private-page.component';
+import { NotfoundPageComponent } from './componentes/notfound-page/notfound-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivatePageComponent,
+    NotfoundPageComponent
   ],
   imports: [
     BrowserModule,
