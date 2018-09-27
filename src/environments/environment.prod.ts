0aac5e5f73e56a773590d156c36d87756b4f3d9a
Gianni Narracci <gnarracci@gmail.com>
2018-09-27 05:09:24 -0400

    Configuracion del Environment con Firebase para Desarrollo y Producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyC4vHIig5lA0O9QESGJIAOhrzcx_wESW-8",
+    authDomain: "loginapp-b34a5.firebaseapp.com",
+    databaseURL: "https://loginapp-b34a5.firebaseio.com",
+    projectId: "loginapp-b34a5",
+    storageBucket: "loginapp-b34a5.appspot.com",
+    messagingSenderId: "956098645508"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyC4vHIig5lA0O9QESGJIAOhrzcx_wESW-8",
+    authDomain: "loginapp-b34a5.firebaseapp.com",
+    databaseURL: "https://loginapp-b34a5.firebaseio.com",
+    projectId: "loginapp-b34a5",
+    storageBucket: "loginapp-b34a5.appspot.com",
+    messagingSenderId: "956098645508"
+  }
 };
 
 /*
