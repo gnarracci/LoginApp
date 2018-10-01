d8a1f49ceacb83b9662af02b8e5a7ab895ce04bc
Gianni Narracci <gnarracci@gmail.com>
2018-10-01 01:39:35 -0400

    Creación del Servicio Auth y Registro de Usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomePageComponent } from './componentes/home-page/home-page.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { RegisterPageComponent } from './componentes/register-page/register-page.component';
 import { LoginPageComponent } from './componentes/login-page/login-page.component';
 import { PrivatePageComponent } from './componentes/private-page/private-page.component';
 import { NotfoundPageComponent } from './componentes/notfound-page/notfound-page.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from '../environments/environment';
+import { AuthService } from './servicios/auth.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +29,12 @@ import { NotfoundPageComponent } from './componentes/notfound-page/notfound-page
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map, take } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    public afAuth: AngularFireAuth
+  ) { }
+
+  registerUser(email: string, pass: string) {
+    return new Promise((resolve, rejected) =>{
+      this.afAuth.auth.createUserWithEmailAndPassword(email, pass)
+      .then( userData => resolve(userData),
+      err => rejected(err));
+    });
+  }
+
+  loginEmail(email: string, pass: string) {
+    return new Promise((resolve, rejected) =>{
+      this.afAuth.auth.signInWithEmailAndPassword(email, pass)
+      .then( userData => resolve(userData),
+      err => rejected(err));
+    });
+  }
+
+  getAuth() {
+    return this.afAuth.authState.pipe(map( auth => auth ));
+  }
+
+  logout() {
+    return this.afAuth.auth.signOut();
+  }
 }
